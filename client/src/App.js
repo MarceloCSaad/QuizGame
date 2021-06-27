2320bbe6a7116a6fc37b50b629bc99c7b4aa58c6
Marcelo Saad <mc.saad@hotmail.com>
2021-06-27 15:30:39 -0300

    Minor CSS updates

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,5 @@
 import React, {useMemo, useState} from 'react';
 import OptionsForm from './Components/OptionsForm/OptionsForm';
-import Button from './Components/Button/Button';
 import Quiz from './Components/Quiz/Quiz.jsx'
 import './App.css';
 
@@ -9,20 +8,14 @@ function App() {
   const [ gameOptions, setGameOptions ] = useState(null)
   const isGameRunning = useMemo( () => ( gameOptions !== null ), [gameOptions] )
 
-  // Setting handlers:
-
-
-  // Setting Renders:
-  const renderQuiz = () => { return ( isGameRunning )
-    ? <Quiz gameOptions={gameOptions} setGameOptions={setGameOptions} />
-    
-    :<OptionsForm setGameOptions={setGameOptions}/>
-  }
-  
   return (
     <div className="App" >
       <div className="content-wrap">
-        {renderQuiz()}
+        {
+          ( isGameRunning )
+          ? <Quiz gameOptions={gameOptions} setGameOptions={setGameOptions} />
+          : <OptionsForm setGameOptions={setGameOptions}/>
+        }
       </div>
     </div>
   );
